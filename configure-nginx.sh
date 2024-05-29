426b1df4c1600d2107dc0fd26697b94d24b0f48e
ewymz <161576748+ewymz@users.noreply.github.com>
2024-05-30 01:10:32 +0800

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is ElwymJamiePingol-vm$(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Welcome to Azure! My name is elwymjamie-vm$(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
